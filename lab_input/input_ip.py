ab379ff1eea0166040e921860c47542be1d52318
Stephanie Hawley <stephanie.hawley@verizonwireless.com>
2018-12-03 14:17:50 -0500

    lab 7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Written By: Stephanie Hawley
+# Collect IP address information from a user, and display it back to them.
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:') # user_input stores the IPv4 ip address
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
+
+## Collect input from the user
+user_input = input('Please enter vendor name associated with the device:') # user_input stores the vendor name
+
+## Print out the input collected from the user
+print("You told me the vendor name is:" + user_input)
+
+## Collect input from the user
+user_input = input('Please enter purchase date of the device:') # user_input stores the vendor name
+
+## Print out the input collected from the user
+print("You told me the purchase date is:" + user_input)
